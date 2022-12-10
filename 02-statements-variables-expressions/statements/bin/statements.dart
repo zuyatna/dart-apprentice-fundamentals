b3363c2bce9267b87a78ccdbf0683378ddb34f97
zuyatna <zuyatna@gmail.com>
2022-12-10 07:52:05 +0700

    statements

---
+++ 02-statements-variables-expressions/statements/bin/statements.dart
@@ -0,0 +1,24 @@
+// This is a comment. It is not executed.
+
+// This is also a comment,
+// over multiple lines.
+
+/* This is also a comment. Over many..
+many...
+many lines.*/
+
+/* This is a comment.
+/* And inside it is
+another comment. */
+Back to the first. */
+
+/// I am a documentation comment
+/// at your service.
+
+/**
+ * Me, too!
+ */
+
+void main() {
+  print('Hello, Dart Apprentices reader');
+}
