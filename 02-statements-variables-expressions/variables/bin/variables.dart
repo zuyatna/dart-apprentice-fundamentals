e66e082a6c4548bc836aa550e98e526a1f6b69a6
zuyatna <zuyatna@gmail.com>
2022-12-10 12:23:49 +0700

    variables, everything is an object

--- 02-statements-variables-expressions/variables/bin/variables.dart
+++ 02-statements-variables-expressions/variables/bin/variables.dart
@@ -1,3 +1,15 @@
 void main() {
-  print('Hello world!');
+  // Variables
+  int number = 10;
+  number = 15;
+  print(number);
+
+  double apple = 3.14159;
+  print(apple);
+
+  // Everything in Dart is an object
+  print(10.isEven);
+  print(apple.round());
+
+  print('');
 }
