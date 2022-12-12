b6c0241270b75adddcc8ca56057bfcc08f19e923
zuyatna <zuyatna@gmail.com>
2022-12-12 12:41:41 +0700

    complete all challenge

--- 03-types-operations/dynamic-types/bin/dynamic_types.dart
+++ 03-types-operations/dynamic-types/bin/dynamic_types.dart
@@ -1,3 +1,30 @@
 void main() {
-  print('Hello world!');
+  // Object and Dynamic Types
+  dynamic myDynamicVariable = 42;
+  myDynamicVariable = 'hello'; // OK
+  print(myDynamicVariable);
+  print('');
+
+  var myVariable; // default to dynamic
+  myVariable = 42; // OK
+  myVariable = 'hello var'; // OK
+  print(myVariable);
+  print('');
+
+  Object? myObjVar = 42;
+  myObjVar = 'hello obj';
+  print(myObjVar);
+  print('');
+
+  // Challenge #1
+  const grade = 0.2;
+  const homework = 0.3;
+  const exam = 0.5;
+
+  final student = (90 * grade) + (80 * homework) + (94 * exam);
+  print(student.toInt());
+
+  // Challenge #2
+  const value = 10 / 2;
+  print(value);
 }
