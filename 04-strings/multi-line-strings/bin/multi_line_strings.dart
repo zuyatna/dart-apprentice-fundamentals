bdff9cc7e218402c60f2b563b9d8a7a0e62623df
zuyatna <zuyatna@gmail.com>
2022-12-14 12:32:18 +0700

    complete all challenge

--- 04-strings/multi-line-strings/bin/multi_line_strings.dart
+++ 04-strings/multi-line-strings/bin/multi_line_strings.dart
@@ -1,3 +1,5 @@
+import 'package:characters/characters.dart';
+
 void main() {
   const bigString = '''
   You can have a string
@@ -17,8 +19,36 @@ void main() {
   print(twoLines);
   print('');
 
-  var name = 'Suyatna';
   const rawString = r'My name \n is $name';
   print(rawString);
   print('');
+
+  // Inserting Characters From Their Codes
+  print('I \u2764 Dart\u0021');
+  print('I Love \u{1F3AF}');
+  print('');
+
+  // Challenge #1
+  const twoCountries = '🇹🇩🇷🇴';
+  print(twoCountries.runes);
+  print('');
+
+  // Challenge #2
+  const vote = 'Thumbs up! 👍🏽';
+  print('UTF-16 code units are: ${vote.codeUnits.length}');
+  print('Unicode code points are: ${vote.runes.length}');
+  print('Unicode grapheme clusters are: ${vote.characters.length}');
+  print('');
+
+  // Challenge #3
+  var name = 'Ray';
+  name += ' Wenderlich';
+  print(name);
+  print('');
+
+  // Challenge #4
+  const number = 10;
+  const multiplier = 5;
+  final summary = '$number \u00D7 $multiplier = ${number * multiplier}';
+  print(summary);
 }
