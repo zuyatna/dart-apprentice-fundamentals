8bc22ac9ac808d926978ba4b44cd738afef88f91
zuyatna <zuyatna@gmail.com>
2022-12-14 22:50:16 +0700

    boolean logic

---
+++ 05-control-flow/boolean-logic/bin/boolean_logic.dart
@@ -0,0 +1,31 @@
+void main() {
+  // Testing Equality
+  const doesOneEqualTwo = 1 == 2;
+  print(doesOneEqualTwo);
+  print('');
+
+  // Testing Inequality
+  const doesOneNotEqualTwo = (1 != 2);
+  print(doesOneNotEqualTwo);
+
+  const alsoTrue = !(1 == 2);
+  print(alsoTrue);
+  print('');
+
+  // Testing Greater and Less Than
+  const isOneGreaterThatTwo = (1 > 2);
+  const isOneLessThatTwo = (1 < 2);
+  print(isOneGreaterThatTwo);
+  print(isOneLessThatTwo);
+  print('');
+
+  print(1 <= 2);
+  print(2 <= 2);
+  print('');
+
+  print(2 >= 1);
+  print(2 >= 2);
+  print('');
+
+  // AND Operator
+}
