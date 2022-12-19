81e3eb7d4434e57b7dbf7476374b62066fed88da
zuyatna <zuyatna@gmail.com>
2022-12-19 18:04:48 +0700

    Making Named Parameters Default

--- 07-functions/functions/bin/functions.dart
+++ 07-functions/functions/bin/functions.dart
@@ -7,6 +7,18 @@ void main() {
 
   print(fullName('Suyatna', 'Light'));
   print(fullName('Albert', 'Einstein', 'Professor'));
+  print('');
+
+  print(withinTolerance(5));
+  print(withinTolerance(15));
+
+  // What do those three numbers even mean?
+  print(withinTolerance(9, 7, 11));
+  print('');
+
+  print(namingWithinTolerance(value: 9, min: 7, max: 11));
+  print(namingWithinTolerance(value: 9, max: 11, min: 7));
+  print(namingWithinTolerance(value: 9, max: 20));
 }
 
 String compliment(int number) {
@@ -25,4 +37,14 @@ String fullName(String first, String last, [String? title]) {
   } else {
     return '$first $last';
   }
+}
+
+// Providing Default Values
+bool withinTolerance(int value, [int min = 0, int max = 10]) {
+  return min <= value && value <= max;
+}
+
+// Making Named Parameters Default
+bool namingWithinTolerance({required int value, int min = 0, int max = 10}) {
+  return min <= value && value <= max;
 }
