962066273fe429fb1b7f127833d3422584f5e9fe
zuyatna <zuyatna@gmail.com>
2022-12-22 10:41:28 +0700

    forwarding constructor

--- 09-constructor/main-activity/bin/main_activity.dart
+++ 09-constructor/main-activity/bin/main_activity.dart
@@ -2,18 +2,16 @@ void main() {
   final user = User(25, 'Suyatna');
   print(user.toJson());
 
-  final anonymousUser = User.anonymous(26);
+  final anonymousUser = User.anonymous();
   print(anonymousUser.toJson());
 }
 
 class User {
   User(this.id, this.name);
-  User.anonymous(this.id) {
-    name = 'anonymous';
-  }
+  User.anonymous() : this(0, 'anonymous');
 
-  int id = 0;
-  String name = '';
+  int id;
+  String name;
 
   String toJson() {
     return '"id":$id, "name":"$name"';
